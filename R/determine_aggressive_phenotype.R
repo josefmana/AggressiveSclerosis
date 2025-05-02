d34bdf5505de50f90ba76f406a8b5038a8cb48d1
josefmana <josef.mana@protonmail.com>
2025-05-02 10:32:54 +0200

    the first & second steps of the diagnosis

--- R/determine_aggressive_phenotype.R
+++ R/determine_aggressive_phenotype.R
@@ -31,17 +31,59 @@
 determine_aggressive_phenotype <- function(demographics, relapses, edss) {
   # Add time stamps to EDSS assessments:
   d0 <-
-    left_join(edss, demographics |> select(id, onset), by = "id") |>
-    mutate(edss_time = time_length(difftime(visit_date, onset), "years"))
+    left_join(
+      edss,
+      demographics |> select(id, onset),
+      by = "id"
+    ) |>
+    mutate(
+      edss_time = time_length(difftime(visit_date, onset), "years")
+    )
+  # Unique patients IDs:
+  unique_ids <- d0$id |> unique()
   # Print cases with negative disease duration and drop them from further analysis:
   negative_cases <- subset(d0, edss_time < 0)
   print(negative_cases, n = Inf)
   cat("\nThe cases printed above show negative disease
-duration at the time of EDSS assessment. Patients with
-these data are dropped in further analyses.")
+duration at the time of EDSS assessment.
+Patients with these data are dropped in further analyses.\n\n")
   # Drop data of patients with negative cases:
+  N0 <- length(unique(d0$id)) # Original number of patients.
   negative_ids <- unique(negative_cases$id)
-  no_dropped <- length(negative_ids)
+  incl_ids0 <- unique_ids[!(unique_ids %in% negative_ids)]
+  no_dropped0 <- length(negative_ids)
   d1 <- subset(d0, !(id %in% negative_ids))
-  cat(glue::glue("\nDropping {no_dropped} patients due to negative disease duration observations ...\n"))
+  cat(glue::glue(
+"\nDropping {no_dropped0} out of {N0} patients
+due to negative disease duration observations ...\n\n"
+  ))
+  # Criterion i
+  N1 <- length(unique(d1$id))
+  message("\nEvaluating the first criterion - disease duration ≥ 10 years ...\n")
+  incl_ids1 <- # All patients with at least one EDSS recorded at disease duration ≥ 10 years
+    d1 |>
+    filter(edss_time >= 10) |>
+    select(id) |>
+    pull() |>
+    unique()
+  excl_ids1 <- c(na.omit(incl_ids1[!(incl_ids0 %in% incl_ids1)]))
+  no_dropped1 <- length(excl_ids1)
+  d2 <- subset(d1, !(id %in% excl_ids1))
+  cat(glue::glue(
+"\nDropping {no_dropped1} out of {N1} remaining patients
+due to not having any EDSS observation after
+10 years of disease duration ...\n\n"
+  ))
+  # Criterion ii
+  N2 <- length(unique(d2$id))
+  message("\nEvaluating the second criterion - EDSS ≥ 6 within the first 10 years of disease ...\n")
+  incl_ids2 <-
+    d2 |>
+    filter(edss_time < 10 & edss >= 6) |>
+    select(id) |>
+    pull() |>
+    unique()
 }
+
+## ADD PERCENTAGES
+## PREPARE HELPER FUNCTION/S FOR REPEATED ACTIONS
